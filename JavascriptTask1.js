d3567c6c009d696e439a5aa32a59d33b9fe4c49b
Naga Sri Venkat Rentala <venkatrentala2001@gmail.com>
2024-06-22 17:09:42 +0530

    update

--- JavascriptTask1.js
+++ JavascriptTask1.js
@@ -1,7 +1,17 @@
 let username = document.getElementById('email');
 let userpassword= document.getElementById('pass');
+let emailerr= document.getElementById('emailerror');
+let paserr= document.getElementById('passerror');
 function myfunction(event){
   event.preventDefault();
-  console.log(username.value);
-  console.log(userpassword.value);
+  emailerr.innerHTML = "";
+  paserr.innerHTML = "";
+  if (username.value !=""){
+    console.log(username.value);
+    console.log(userpassword.value);
+  }
+  else{
+    emailerr.innerHTML="emailid is required";
+    paserr.innerHTML="password is required";
+  }
 }
