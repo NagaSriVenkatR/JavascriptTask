e38a3e3a0d23887e1375bc256d7f2d72ce5264b0
Naga Sri Venkat Rentala <venkatrentala2001@gmail.com>
2024-06-24 17:25:55 +0530

    update

--- JavascriptTask1.js
+++ JavascriptTask1.js
@@ -6,12 +6,47 @@ function myfunction(event){
   event.preventDefault();
   emailerr.innerHTML = "";
   paserr.innerHTML = "";
-  if (username.value !=""){
-    console.log(username.value);
-    console.log(userpassword.value);
+  let emailPattern = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,20})$/;
+  let upperCasePattern = '/^(&=.*[A-Z])$/';
+  let lowerCasePattern = '/^(&=.*[a-z])$/';
+  let numberPatten = '/^(&=.*[0-9])$/';
+  let specialCharacterPatten = '/^(&=.*[~!@#$%^&()_?])$/';
+  let minlengthCharacterPattern = '/^[A-Za-z0-9~!@#$%^&()_?]{8,16}$/';
+  // let passwordPattern = new RegExp(`^${upperCasePattern}${lowerCasePattern}${numberPatten}${specialCharacterPatten}${minlengthCharacterPattern}$`);
+  let email = username.value;
+  let password = userpassword.value ;
+  if (email === "") {
+    emailerr.innerHTML = "Email is required";
+  } else if (!emailPattern.test(email)) {
+    emailerr.innerHTML = "Invalid email format";
   }
-  else{
-    emailerr.innerHTML="emailid is required";
-    paserr.innerHTML="password is required";
+  if (password === "") {
+    paserr.innerHTML = "Password is required";
+  } else {
+    let isValid = true;
+    if(!minlengthCharacterPattern.test(password)){
+      paserr.innerHTML += "Password must be between 8 and 16 characters <br>";
+      isValid = false;
+    }
+    if(!upperCasePattern.test(password)){
+      paserr.innerHTML += "atleast 1 Uppercase character <br>";
+      isValid = false;
+    }
+    if(!lowerCasePattern.test(password)){
+      paserr.innerHTML += "atleast 1 Lowercase character <br>";
+      isValid = false;
+    }
+    if(!numberPatten.test(password)){
+      paserr.innerHTML = "atleast 1 number <br>";
+      isValid = false;
+    }
+    if(!specialCharacterPatten.test(password)){
+      paserr.innerHTML = "atleast 1 special character <br>";
+      isValid = false;
+    }
+  }
+  if (emailPattern.test(email) && passwordPattern.test(password)) {
+    console.log("Email:", email);
+    console.log("Password:", password); 
   }
 }
