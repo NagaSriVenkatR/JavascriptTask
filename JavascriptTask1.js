9ffb4643668f0c3c30dc2234980d659bc48ad988
Naga Sri Venkat Rentala <venkatrentala2001@gmail.com>
2024-06-25 16:52:26 +0530

    updated to displa table

--- JavascriptTask1.js
+++ JavascriptTask1.js
@@ -7,46 +7,64 @@ function myfunction(event){
   emailerr.innerHTML = "";
   paserr.innerHTML = "";
   let emailPattern = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,20})$/;
-  let upperCasePattern = '/^(&=.*[A-Z])$/';
-  let lowerCasePattern = '/^(&=.*[a-z])$/';
-  let numberPatten = '/^(&=.*[0-9])$/';
-  let specialCharacterPatten = '/^(&=.*[~!@#$%^&()_?])$/';
-  let minlengthCharacterPattern = '/^[A-Za-z0-9~!@#$%^&()_?]{8,16}$/';
-  // let passwordPattern = new RegExp(`^${upperCasePattern}${lowerCasePattern}${numberPatten}${specialCharacterPatten}${minlengthCharacterPattern}$`);
+  let upperCasePattern = /[A-Z]/;
+  let lowerCasePattern = /[a-z]/;
+  let numberPatten = /[0-9]/;
+  let specialCharacterPatten = /[~!@#%&()$^_?]/;
+  let minlengthCharacterPattern = /^.{8,16}$/;
   let email = username.value;
   let password = userpassword.value ;
   if (email === "") {
     emailerr.innerHTML = "Email is required";
+    return;
   } else if (!emailPattern.test(email)) {
     emailerr.innerHTML = "Invalid email format";
+    return;
   }
   if (password === "") {
     paserr.innerHTML = "Password is required";
+    return;
   } else {
-    let isValid = true;
     if(!minlengthCharacterPattern.test(password)){
       paserr.innerHTML += "Password must be between 8 and 16 characters <br>";
-      isValid = false;
+      return ;
     }
-    if(!upperCasePattern.test(password)){
+    else if(!upperCasePattern.test(password)){
       paserr.innerHTML += "atleast 1 Uppercase character <br>";
-      isValid = false;
+      return ;
     }
-    if(!lowerCasePattern.test(password)){
+    else if(!lowerCasePattern.test(password)){
       paserr.innerHTML += "atleast 1 Lowercase character <br>";
-      isValid = false;
+      return ;
     }
-    if(!numberPatten.test(password)){
+    else if(!numberPatten.test(password)){
       paserr.innerHTML = "atleast 1 number <br>";
-      isValid = false;
+      return ;
     }
-    if(!specialCharacterPatten.test(password)){
+    else if(!specialCharacterPatten.test(password)){
       paserr.innerHTML = "atleast 1 special character <br>";
-      isValid = false;
+      return ;
     }
+    // else {
+    //   // console.log("Email:", email);
+    //   // console.log("Password:", password); 
+    //   // return true;
+     
+    // } 
   }
-  if (emailPattern.test(email) && passwordPattern.test(password)) {
-    console.log("Email:", email);
-    console.log("Password:", password); 
-  }
+
+  let dataTable = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+  let newRow = dataTable.insertRow(dataTable.rows.length);
+
+  let cell1 = newRow.insertCell(0);
+  let cell2 = newRow.insertCell(1);
+  cell1.innerHTML = email;
+  cell2.innerHTML = password;
+
+  console.log("Email:", email);
+  console.log("Password:", password);
+
+
+  document.getElementById('myform').reset();
+   
 }
