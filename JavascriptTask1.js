2276d1080e93dc33a4168f14a8797a74c7610e2e
Naga Sri Venkat Rentala <venkatrentala2001@gmail.com>
2024-06-26 16:44:44 +0530

    Using CRUD operations to display table in html page by using javascript

--- JavascriptTask1.js
+++ JavascriptTask1.js
@@ -1,19 +1,22 @@
-let username = document.getElementById('email');
-let userpassword= document.getElementById('pass');
-let emailerr= document.getElementById('emailerror');
-let paserr= document.getElementById('passerror');
-function myfunction(event){
+let data = [];
+let editIndex = -1;
+let username = document.getElementById("email");
+let userpassword = document.getElementById("pass");
+let emailerr = document.getElementById("emailerror");
+let paserr = document.getElementById("passerror");
+
+function myfunction(event) {
   event.preventDefault();
   emailerr.innerHTML = "";
   paserr.innerHTML = "";
   let emailPattern = /^([a-zA-Z0-9\.-]+)@([a-zA-Z0-9-]+).([a-z]{2,20})$/;
   let upperCasePattern = /[A-Z]/;
   let lowerCasePattern = /[a-z]/;
-  let numberPatten = /[0-9]/;
-  let specialCharacterPatten = /[~!@#%&()$^_?]/;
+  let numberPattern = /[0-9]/;
+  let specialCharacterPattern = /[~!@#%&()$^_?]/;
   let minlengthCharacterPattern = /^.{8,16}$/;
   let email = username.value;
-  let password = userpassword.value ;
+  let password = userpassword.value;
   if (email === "") {
     emailerr.innerHTML = "Email is required";
     return;
@@ -25,34 +28,56 @@ function myfunction(event){
     paserr.innerHTML = "Password is required";
     return;
   } else {
-    if(!minlengthCharacterPattern.test(password)){
+    if (!minlengthCharacterPattern.test(password)) {
       paserr.innerHTML += "Password must be between 8 and 16 characters <br>";
-      return ;
-    }
-    else if(!upperCasePattern.test(password)){
-      paserr.innerHTML += "atleast 1 Uppercase character <br>";
-      return ;
-    }
-    else if(!lowerCasePattern.test(password)){
-      paserr.innerHTML += "atleast 1 Lowercase character <br>";
-      return ;
-    }
-    else if(!numberPatten.test(password)){
-      paserr.innerHTML = "atleast 1 number <br>";
-      return ;
-    }
-    else if(!specialCharacterPatten.test(password)){
-      paserr.innerHTML = "atleast 1 special character <br>";
-      return ;
+      return;
+    } else if (!upperCasePattern.test(password)) {
+      paserr.innerHTML += "at least 1 Uppercase character <br>";
+      return;
+    } else if (!lowerCasePattern.test(password)) {
+      paserr.innerHTML += "at least 1 Lowercase character <br>";
+      return;
+    } else if (!numberPattern.test(password)) {
+      paserr.innerHTML += "at least 1 number <br>";
+      return;
+    } else if (!specialCharacterPattern.test(password)) {
+      paserr.innerHTML += "at least 1 special character <br>";
+      return;
     }
   }
-  let dataTable = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
-  let newRow = dataTable.insertRow(dataTable.rows.length);
-  let cell1 = newRow.insertCell(0);
-  let cell2 = newRow.insertCell(1);
-  cell1.innerHTML = email;
-  cell2.innerHTML = password;
-  console.log("Email:", email);
-  console.log("Password:", password);
-  document.getElementById('myform').reset();   
+  if (editIndex === -1) {
+    data.push({ email, password });
+  } else {
+    data[editIndex] = { email, password };
+    editIndex = -1;
+  }
+  updateTable();
+  document.getElementById("myform").reset();
+}
+function updateTable() {
+  let dataTable = document
+    .getElementById("dataTable")
+    .getElementsByTagName("tbody")[0];
+  dataTable.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    let newRow = dataTable.insertRow(dataTable.rows.length);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    let cell3 = newRow.insertCell(2);
+    let cell4 = newRow.insertCell(3);
+    cell1.innerHTML = i + 1;
+    cell2.innerHTML = data[i].email;
+    cell3.innerHTML = data[i].password;
+    cell4.innerHTML = `<button onclick="editRow(${i})" style="padding:5px 15px;background-color:purple;color:white;font-size:20px;border-radius:5px">Update</button>
+                           <button onclick="deleteRow(${i})" style="padding:5px 15px;background-color:Red;color:white;font-size:20px;border-radius:5px">Delete</button>`;
+  }
+}
+function updateRow(index) {
+  username.value = data[index].email;
+  userpassword.value = data[index].password;
+  editIndex = index;
+}
+function deleteRow(index) {
+  data.splice(index, 1);
+  updateTable();
 }
